4a3e227a44d3fb2906378d5bf5ffc73076223f8b
Ryuzaki <bpow2308@gmail.com>
2024-09-03 10:58:20 +0800

    Finish HomePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./index.html"],
-	theme: {
-		extend: {
-			fontFamily: {
-				libre_franklin: ["Libre Franklin", "san-serif"],
-			},
-		},
-	},
-	plugins: [],
-};
+    content: ["./src/**/*.{html,js}", "./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                libre_franklin: ["Libre Franklin", "san-serif"],
+            },
+
+            fontSize: {
+                xsm: "0.50rem",
+            },
+        },
+    },
+    plugins: [],
+}
